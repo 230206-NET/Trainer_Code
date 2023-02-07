85d310b55192c9cbb024da1dede13b1b860e38a9
Richard Hawkins <Richard.Hawkins@revature.com>
2023-02-07 12:57:42 -0500

    add challenges

---
+++ wk1/CodeChallenges/02_DataTypeAndVariables/DataTypeAndVariables/Program.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace DataTypeAndVariables
+{
+    public class Program
+    {
+      public static void Main(string[] args)
+      {
+          Console.WriteLine("Hello World!");
+
+          byte myByte;
+          sbyte mySbyte;
+          int myInt;
+          uint myUint;
+          short myShort;
+          ushort myUShort;
+          float myFloat;
+          double myDouble;
+          char myCharacter;
+          bool myBool;
+          string myText;
+          string numText;
+      }
+
+      public static int Text2Num(string numText)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}
