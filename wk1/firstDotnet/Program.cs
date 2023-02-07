bd558f533211353493747488180dd9d48b7afab5
Richard Hawkins <Richard.Hawkins@revature.com>
2023-02-07 12:46:47 -0500

    Add firstDotnet example

---
+++ wk1/firstDotnet/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Starting Program...");
+
+string userInput = Console.ReadLine();
+
+Console.WriteLine(userInput);
+
+
+if (5>4) 
+{
+	Console.WriteLine("Five is greater than 4!");
+}
+
+if (4>5) 
+{
+	Console.WriteLine("Four is greater than 5!");
+}
+
+
+int balance = 0;
+
+if (Balance <= 0)
+{
+	Console.WriteLine("Account Balance Must not have a negative balance!");
+}
+
+else 
+{
+	Console.WriteLine(balance);
+}
+
+
+
+
+
+
+Console.WriteLine("Ending Program");
