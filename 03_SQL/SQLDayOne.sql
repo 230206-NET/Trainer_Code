4da501ef72319422a9f6469e1f51fdccb9cba0b2
Minseon Song <songminseonk@gmail.com>
2023-02-20 12:23:49 -0500

    more sql notes

--- 03_SQL/SQLDayOne.sql
+++ 03_SQL/SQLDayOne.sql
@@ -12,29 +12,39 @@
 -- );
 -- CREATE keyword is part of Data Definition Language(DDL) which contains keywords to define your data
 -- use DROP keyword to completely remove the resource (table, etc) with no trace remaining 
+Drop Table Exercises;
 Drop Table WorkoutSessions;
 Truncate Table WorkoutSessions;
+
+-- Primary Key is a set of one or more columns we use to uniquely identify a row in a table
+-- They can't be duplicates nor they can be left empty
+
+-- Constraints further define the valid dataset in your table
+-- some examples of constraints are: primary key, foreign key, not null, unique, default, check, etc.
 Create Table WorkoutSessions (
-    WorkoutDate datetime,
-    WorkoutName nvarchar(50)
+    Id int PRIMARY KEY IDENTITY(1, 1),
+    WorkoutDate datetime default getdate(),
+    WorkoutName nvarchar(50) default getdate()
 );
 
 Create Table Exercises (
-    ExerciseName varchar(100),
-    ExerciseNote varchar(255)
+    Id int PRIMARY KEY IDENTITY(1, 1),
+    ExerciseName varchar(100) not null,
+    ExerciseNote varchar(255),
+    WorkoutId int FOREIGN KEY REFERENCES WorkoutSessions(Id)
 );
 
 -- To add a row to table, use INSERT keyword
-INSERT INTO WorkoutSessions(WorkoutDate, WorkoutName) VALUES ('2/19/23 1pm', 'mt tabor hike');
-
+INSERT INTO WorkoutSessions(WorkoutDate) Values (getdate());
+INSERT INTO Exercises(ExerciseName, ExerciseNote, WorkoutId) Values ('jog', '1mi along the creek', 2);
 -- To retrieve the data use SELECT keyword
-SELECT WorkoutDate, WorkoutName FROM WorkoutSessions;
+SELECT Id, WorkoutDate, WorkoutName FROM WorkoutSessions;
 
 -- Get all the columns from the following table
 SELECT * FROM WorkoutSessions;
-
+select * from Exercises;
 -- use Delete keyword to get rid of 1 or more specific rows
-DELETE FROM WorkoutSessions WHERE WorkoutName = 'mt tabor hike';
+DELETE FROM WorkoutSessions WHERE Id = 1;
 
 -- INSERT / Delete is part of Data Manipulation Language (DML) which modifies one or more rows
 
